5dc9ae67f112c2f35bbdc71369a9ee9f239c9af9
Al Jerone <ajgimpayan@gmail.com>
2023-02-22 08:38:00 +0000

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
+import airbnbLogo from "../images/airbnb-logo.png";
 
 export default function Navbar() {
   return (
     <nav>
-      <img src="../images/airbnb-logo.png" />
+      <img src={airbnbLogo} />
     </nav>
   );
 }
